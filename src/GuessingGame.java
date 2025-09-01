1ce56aac5a4d7fe434cb999b2784f46acf1498f2
Bluiss <135335614+Bluiss@users.noreply.github.com>
2025-09-01 17:17:41 +0100

    add guessing logic and user input

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -7,6 +7,29 @@ public class GuessingGame {
         Random rand = new Random();
         int guess = rand.nextInt(101);
 
+        int K = 10; // Maximum attempts
 
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i <= K; i++) {
+            System.out.println("Guess a number between 0 and 100, you have " + (K - i + 1) + " attempts left");
+            int number = input.nextInt();
+
+            if(number > guess) {
+                System.out.println("Go lower, try again");
+            }
+            else if(number < guess) {
+                System.out.println("Go higher, try again");
+            }
+            else if(number == guess) {
+                System.out.println("Well done! You guessed it in " + i + " attempts!");
+                input.close();
+                return;
+            }
+        }
+
+        // This only executes if the loop completes without finding the answer
+        System.out.println("You've exhausted all attempts. The correct number was: " + guess);
+        input.close();
     }
 }
